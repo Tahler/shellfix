dd9014fb7afda2708023e1c5fbd90fc40f5e8ad2
Tyler Berry <tyler@berryac.com>
2018-06-05 13:35:14 -0700

    Run fixers as array

--- fixer/__init__.py
+++ fixer/__init__.py
@@ -6,7 +6,8 @@ from . import sc2006
 from . import sc2086
 
 def fix_all(path: str):
-    sc2006.fix(path)
-    sc2086.fix(path)
+    fixers = [sc2006, sc2086]
+    for fixer in fixers:
+        fixer.fix(path)
     # fix 2016
     # fix 2046
