bf872315b026711275b3edd56a3eb6eb0afcd283
Tyler Berry <tyler@berryac.com>
2018-06-05 13:36:20 -0700

    Catch and log exceptions

--- fixer/__init__.py
+++ fixer/__init__.py
@@ -5,9 +5,13 @@ from . import shellcheck
 from . import sc2006
 from . import sc2086
 
+
 def fix_all(path: str):
     fixers = [sc2006, sc2086]
     for fixer in fixers:
+        try:
         fixer.fix(path)
+        except Exception as e:
+            logging.error(e)
     # fix 2016
     # fix 2046
