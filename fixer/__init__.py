b7bbba7b008beeccd55004674d26971c0c28b947
Tyler Berry <tyler@berryac.com>
2018-06-06 15:34:42 -0700

    Add SC1117

--- fixer/__init__.py
+++ fixer/__init__.py
@@ -2,6 +2,7 @@ import logging
 import sys
 
 from . import shellcheck
+from . import sc1117
 from . import sc2006
 from . import sc2016
 from . import sc2034
@@ -13,7 +14,17 @@ from . import sc2181
 
 
 def fix_all(path: str):
-    fixers = [sc2006, sc2016, sc2034, sc2035, sc2046, sc2086, sc2155, sc2181]
+    fixers = [
+        sc1117,
+        sc2006,
+        sc2016,
+        sc2034,
+        sc2035,
+        sc2046,
+        sc2086,
+        sc2155,
+        sc2181,
+    ]
     for fixer in fixers:
         try:
             fixer.fix(path)
